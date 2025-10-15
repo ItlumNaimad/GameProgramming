70eaf044e2a03259bd2d65b52131e860e5c380eb
ItlumNaimad <naimad2003@wp.pl>
2025-10-16 00:40:03 +0200

    Update palette.gd

--- lab1/scripts/palette.gd
+++ lab1/scripts/palette.gd
@@ -2,10 +2,10 @@ extends CharacterBody2D
 
 @onready var ball: CharacterBody2D = $"../ball"
 
-var speed: float = 120
+var speed: float = 180
 # _process działa 8 razy wolniej niż _physics_process
-func _process(delta):
+func _physics_process(delta):
+	# przesuwanie paletki z pominięciem fizyki silnika
 	global_position.y = move_toward(global_position.y, ball.global_position.y, speed*delta)
-	move_and_slide()
 	
 	
